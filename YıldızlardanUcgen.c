be351b7b7a4d98a8e8c728d65081f1b775164338
alpborakorkmaz <67584673+alpborakorkmaz@users.noreply.github.com>
2020-07-03 11:27:45 +0300

    Create YıldızlardanUcgen.c

---
+++ YıldızlardanUcgen.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	int a,i,b;
+	printf("Lutfen bir sayi giriniz:");
+	scanf("%d",&a);
+	for(b=0;b<a;b++){
+		for(i=0;i<=b;i++){
+			printf("*");
+		}
+		printf("\n");
+	}
+	
+	
+	
+	
+}
