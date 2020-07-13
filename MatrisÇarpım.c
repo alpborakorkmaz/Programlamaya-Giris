c61a8deb02d596bd62eccb093b09c621da2c8d4a
alpborakorkmaz <67584673+alpborakorkmaz@users.noreply.github.com>
2020-07-13 14:28:20 +0300

    Create MatrisÇarpım.c

---
+++ MatrisÇarpım.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+	int i,j;
+	int dizibir[3][3];
+	int diziiki[3][3];
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			scanf("%d",&dizibir[i][j]);
+		}
+	}
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			scanf("%d",&diziiki[i][j]);
+			diziiki[i][j]*=dizibir[i][j];
+		}
+	}
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			printf("%d ",diziiki[i][j]);
+		}
+		printf("\n");
+	}
+}
